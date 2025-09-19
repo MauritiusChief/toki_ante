f9ddf7994e33a9d24505478427436d664b7b2b4b
MauritiusChief <107936497+MauritiusChief@users.noreply.github.com>
2025-09-19 16:22:39 -0400

    友好词典

--- preset_dictionary_generater.py
+++ preset_dictionary_generater.py
@@ -25,8 +25,45 @@ changes_d = {
         "pi": "噼"
     }
 }
+changes_f = {
+    "outfile": "dictionary_f.csv",
+    "edits": {
+        "ala": "无不非否",
+        "e": "把将",
+        "ijo": "什物",
+        "ike": "坏歹",
+        "kalama": "声音",
+        "kama": "来至到",
+        "kasi": "草木",
+        "ken": "可能",
+        "kili": "果蔬",
+        "kin": "亦也",
+        "kiwen": "石硬",
+        "laso": "蓝兰",
+        "lawa": "首头",
+        "li": "者兮",
+        "loje": "红丹",
+        "luka": "手五",
+        "lukin": "看见",
+        "mi": "吾我",
+        "o": "乎请",
+        "pali": "工作做造",
+        "pan": "米面",
+        "pana": "出予",
+        "pona": "良好",
+        "sewi": "上天",
+        "sike": "年轮",
+        "sina": "你尔",
+        "sitelen": "图书",
+        "taso": "但惟",
+        "toki": "语言话",
+        "tawa": "向往",
+        "utala": "战斗",
+        "weka": "离去"
+    }
+}
 
-changes = [changes_c, changes_d]
+changes = [changes_c, changes_d, changes_f]
 
 # 读取原始 CSV 内容
 with input_file.open("r", encoding="utf-8-sig", newline="") as f:
